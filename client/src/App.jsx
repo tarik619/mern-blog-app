c54aae221e025a4de9a00bc20b1c7bdfcbcf0857
tarik619 <itarik707@gmail.com>
2024-01-25 19:46:15 +0600

    install vite react and tailwind css satrter template

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="">
+        <h1 className="text-red-400 text-3xl uppercase">hi!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
