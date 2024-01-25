92e59b53fb92fb1fca1da0e4e2042d0606636c6c
tarik619 <itarik707@gmail.com>
2024-01-25 22:10:32 +0600

    create header using flowbite-react make some changes in tailwind.config file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
-import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
