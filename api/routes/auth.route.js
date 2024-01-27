bb7ced9421ba3f1780e671b955359970d4be1507
tarik619 <itarik707@gmail.com>
2024-01-27 13:48:02 +0600

    complete sign in functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,6 +4,6 @@ import { signIn, signUp } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signUp);
-router.get("/signin", signIn);
+router.post("/signin", signIn);
 
 export default router;
