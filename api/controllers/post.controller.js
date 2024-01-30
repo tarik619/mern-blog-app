2d1e7b3a64780b1098f78a03f6007cd7aeda3fcf
tarik619 <itarik707@gmail.com>
2024-01-30 19:40:23 +0600

    add create a post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,27 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const createPost = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "you are not allowed to create post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "title and content are required"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savepost = await newPost.save();
+    res.status(201).json(savepost);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -27,6 +28,7 @@ app.listen(3000, () => {
 app.use("/api/user", userRoute);
 
 app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
