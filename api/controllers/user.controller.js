7993bd8d4d6668a9a223b96dc75fa2726b0e80dc
tarik619 <itarik707@gmail.com>
2024-01-30 10:59:25 +0600

    add deleteuser api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,3 +52,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(
+      errorHandler(403, "you are not allowed to delete this profile")
+    );
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
