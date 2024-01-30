e22e12f307195ff34cdf5171f7d553f618af9a3a
tarik619 <itarik707@gmail.com>
2024-01-30 12:31:12 +0600

    create onlyadminprivate route for creting post add to app.jsx

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -22,6 +22,7 @@ import {
   signOutSuccess,
 } from "../redux/user/userSlice";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
   const { currentUser, error, loading } = useSelector((state) => state.user);
@@ -235,9 +236,25 @@ export default function DashProfile() {
           id="password"
           placeholder="*******"
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? "Loading..." : "Update"}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={"/create-post"}>
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="w-full"
+            >
+              Create a Post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-3 ">
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
