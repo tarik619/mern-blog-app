def09f1a5d7201d52ec3e61a10e53f71b680e49a
tarik619 <itarik707@gmail.com>
2024-02-05 23:52:20 +0600

    complete about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="font-semibold my-7 text-3xl text-center ">
+            About Tarik's Blog
+          </h1>
+          <div className="text-md text-gray-600 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, dignissimos corporis, dolorum sequi accusamus
+              tenetur dicta doloremque nam illum animi officia, est asperiores
+              quod voluptatibus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, dignissimos corporis, dolorum sequi accusamus
+              tenetur dicta doloremque nam illum animi officia, est asperiores
+              quod voluptatibus!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, dignissimos corporis, dolorum sequi accusamus
+              tenetur dicta doloremque nam illum animi officia, est asperiores
+              quod voluptatibus!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold ">Projects</h1>
+      <p className="text-base text-gray-600">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo similique
+        necessitatibus, facilis error reprehenderit culpa?
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
